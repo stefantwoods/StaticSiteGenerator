eca972fd6b2066e74416dca4febc0c21bf2db941
stefantwoods <stefan5365@hotmail.com>
2025-01-26 18:17:18 -0700

    added htmlnode class and testing

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextNode, TextType
 
 
 def main():
-    obj = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+    obj = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(obj)
 if __name__ == "__main__":
     main()
