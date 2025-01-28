e202d91f369e2a1a5a2398a3cf2a30784094cbaa
stefantwoods <stefan5365@hotmail.com>
2025-01-27 21:19:20 -0700

    added leafnode child class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,34 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, children=None, props=None):
+        super().__init__(tag, value, children=None, props=None)
+
+        if self.children != None:
+            raise Exception("LeafNode shouldn't have children")
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have value")
+        
+        if self.tag == None:
+            return self.value
+        
+        if self.tag == "p":
+            return f"<p>{self.value}</p>"
+        elif self.tag == "b":
+            return f"<b>{self.value}</b>"
+        elif self.tag == "i":
+            return f"<i>{self.value}</i>"
+        elif self.tag == "code":
+            return f"<code>{self.value}</code>"
+        elif self.tag == "a":
+            return f"<a href=\"{self.prop["href"]}\">{self.value}</a>"
+        elif self.tag == "img":
+            return f"<img src=\"{self.prop["src"]}\">"
+        else:
+            raise ValueError("not a tag")
+
+        
